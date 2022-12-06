f601cbe63eec08b0c90ffd39a3c962609257fe8a
Seda Özer <86311352+sedaozer4@users.noreply.github.com>
2022-12-06 20:20:39 +0300

    Update DatabaseServices.dart

--- lib/Services/DatabaseServices.dart
+++ lib/Services/DatabaseServices.dart
@@ -1,7 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import '../Constants/Constants.dart';
 import 'package:twitter/Models/Activity.dart';
-import 'package:twitter/Models/Tweet.dart';
 import '../Models/UserModel.dart';
 
 class DatabaseServices {
